874efa3ff1d71508df1c7d1c5b13dee4eb29e62e
wxllxngton <jjngugi27@gmail.com>
2024-03-04 12:35:46 +0300

    Modified: app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,35 +1,38 @@
 #!/usr/bin/python3
-"""
-Script contains the Airbnb Clone API.
-"""
-from os import getenv
-from flask import Flask
-from flask_cors import CORS
+"""app"""
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+
 
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def close_session(exception):
-    """
-    Teardown method to close the storage session.
-    """
+def tear(self):
+    ''' closes storage engine '''
     storage.close()
 
 
-if __name__ == "__main__":
-    """
-    Run the Flask server.
-    """
-    # Get environment variables
-    host = getenv("HBNB_API_HOST") or '0.0.0.0'
-    port = int(getenv("HBNB_API_PORT") or 5000)
+@app.errorhandler(404)
+def not_found(error):
+    ''' handles 404 error and gives json formatted response '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-    app.run(host=host, port=port, debug=True, threaded=True)
+if __name__ == '__main__':
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
+    else:
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
+    else:
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
