81b060566e085ae08c17add1ce16fc7acf0011de
wxllxngton <jjngugi27@gmail.com>
2024-03-05 11:21:16 +0300

    Fixed: Some minor bugs in users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 """
 Module containing views for User objects.
-
-This module defines Flask views for handling User objects,
-including listing users, retrieving user details,
-creating, updating, and deleting users.
 """
 from api.v1.views import app_views
 from flask import jsonify, abort, request
---
+++ test_doc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Testing documentation of a module
+"""
+from importlib import import_module
+import sys
+import os
+
+# Check if the correct number of command-line arguments is provided
+if len(sys.argv) != 2:
+    print("Usage: python script.py <module_name>")
+    sys.exit(1)
+
+module_name = os.path.splitext(os.path.basename(sys.argv[1]))[0]
+module_path = os.path.dirname(os.path.abspath(sys.argv[1]))
+
+# Add the module's directory to the Python path
+sys.path.append(module_path)
+
+# Use the module name in your script
+m_imported = import_module(module_name)
+
+if m_imported.__doc__ is None:
+    print("No module documentation", end="")
+else:
+    print("OK", end="")
+
